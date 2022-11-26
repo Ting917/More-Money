62a75590bff8f4b0a329119c42e7f29c1fe14904
Ting <1353008829@qq.com>
2022-11-26 15:27:43 +0800

    成功全部使用 Vuex 来进行数据管理

--- src/components/Money/FormItem.vue
+++ src/components/Money/FormItem.vue
@@ -1,46 +1,44 @@
 <template>
-    <div>
-        <label class="formItem">
-            <span class="name">{{this.fieldName}}</span>
-            <input type="text" :value="value" @input="onValueChanged($event.target.value)" :placeholder="placeholder">
-        </label>
-    </div>
+  <div>
+    <label class="formItem">
+      <span class="name">{{this.fieldName}}</span>
+      <input type="text"
+             :value="value"
+             @input="onValueChanged($event.target.value)"
+             :placeholder="placeholder">
+    </label>
+  </div>
 </template>
 
 <script lang="ts">
-import Vue from "vue";
-import {Component, Watch, Prop} from "vue-property-decorator";
-@Component
-    export default class FormItem extends Vue {
-        @Prop({default: ''}) readonly value!: string;
-        
-        @Prop({required: true}) fieldName!: string;
-        @Prop() placeholder?: string;
-
-
-        // @Watch('value')
-        onValueChanged(value: string){
-            this.$emit('update:value', value)
-        }
+  import Vue from 'vue';
+  import {Component, Prop, Watch} from 'vue-property-decorator';
+  @Component
+  export default class FormItem extends Vue {
+    @Prop({default: ''}) readonly value!: string;
+    @Prop({required: true}) fieldName!: string;
+    @Prop() placeholder?: string;
+    onValueChanged(value: string) {
+      this.$emit('update:value', value);
     }
+  }
 </script>
 
 <style lang="scss" scoped>
-.formItem {
+  .formItem {
     font-size: 14px;
     padding-left: 16px;
-    display: block;
     display: flex;
     align-items: center;
-    > .name {
-        padding-right: 16px;
+    .name {
+      padding-right: 16px;
     }
     input {
-        height: 40px;
-        flex-grow: 1;
-        background: transparent;
-        border: none;
-        padding-right: 16px;
+      height: 40px;
+      flex-grow: 1;
+      background: transparent;
+      border: none;
+      padding-right: 16px;
     }
-}
+  }
 </style>
