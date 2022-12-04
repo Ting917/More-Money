95889c9524b6885d1bbc832ae271453c7011d9b5
Ting <1353008829@qq.com>
2022-12-04 17:20:17 +0800

    修复了很多小问题

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -3,9 +3,9 @@
         <NumberPad @update:value="onUpdateAmount" @submit="saveRecord"/>
         <Tabs :data-source="recordTypeList" :value.sync="record.type"/>
         <div class="notes">
-        <FormItem field-name="备注" placeholder="在这里输入备注" @update:value="onUpdateNotes"/>
+        <FormItem field-name="备注" placeholder="在这里输入备注" :value.sync="record.notes" />
         </div>
-        <Tags /> 
+        <Tags @update:value='record.tags = $event'/> 
     </Layout>
 </template>
 
@@ -32,7 +32,7 @@
         }
         recordTypeList = recordTypeList;
         created(){
-            this.$store.commit('fetchRecords')
+            this.$store.commit('fetchRecords');
         }
         onUpdateNotes(value: string) {
             this.record.notes = value;
@@ -41,7 +41,14 @@
             this.record.amount = parseFloat(value);
         }
         saveRecord(){
+            if(!this.record.tags || this.record.tags.length === 0){
+                return window.alert('请至少选择一个标签')
+            }
             this.$store.commit('createRecord', this.record);
+            if(this.$store.state.createRecordError === null) {
+                window.alert('已保存');
+                this.record.notes = '';
+            }
         }
     }
 </script>
